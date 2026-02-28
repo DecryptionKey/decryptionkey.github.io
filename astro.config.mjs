2c1a4eb5d1584bef957e377db93862d97a87c119
DecryptionKey <118622913+DecryptionKey@users.noreply.github.com>
2026-02-27 21:13:35 -0500

    update site url to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://decryptionkey.github.io',
+  site: 'https://manyewade.com',
   vite: {
     plugins: [tailwindcss()],
   },
